dcae962cb36d10d266b4fc6c25e1f0585b1d8fc0
saber zaabouti <saberzaabouti@gmail.com>
2018-03-28 16:58:53 +0200

    ajout des routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 
 import { AppComponent } from './app.component';
@@ -7,17 +9,28 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductsService } from './services/products.service';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
+const routes = [
+  {path : 'list', component : ProductsListComponent},
+  {path : 'welcome', component : WelcomeComponent},
+  {path : '', redirectTo: 'welcome', pathMatch: 'full'},
+ {path : '**', component : NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductsListComponent,
-    FooterComponent
+    WelcomeComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, // bootsraper l'application dans navigateur web
+    RouterModule.forRoot(routes)
   ],
   providers: [
     ProductsService // service pour tous les composants
